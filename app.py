bd9ced8a05c59cbfa6f31162f4d07af5b3a189eb
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2022-12-17 18:35:52 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,28 @@
 from spleeter.separator import Separator
-from flask import Flask
+from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
 
-@app.route('/stems/2')
-def two_stems():  # put application's code here
-    return stems()
+@app.route('/cli', methods=['POST'])
+def process_audio_cli():
+    request_data = request.get_json()
+
+    os.system('spleeter separate -p ' + request_data['stems'] + ' -o ' + request_data['folder'] + ' ' + request_data['file'])
+
+    return 'audio processed', 201
 
 
-def stems():
-    # Using embedded configuration.
-    separator = Separator('spleeter:4stems')
+@app.route('/library', methods=['POST'])
+def process_audio_library():
+    request_data = request.get_json()
 
-    audio_file = "audio_example.mp3"
-    folder = "output4"
-    separator.separate_to_file(audio_file, folder)
+    separator = Separator(request_data['stems'])
 
-    return "4 files processed"
+    separator.separate_to_file(request_data['file'], request_data['folder'])
+
+    return 'audio processed', 201
 
 
 if __name__ == '__main__':
